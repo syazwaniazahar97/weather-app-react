500acf0270cca64d253b6d68b61f3044546f373b
syazwaniazahar97 <92834414+syazwaniazahar97@users.noreply.github.com>
2024-02-05 20:39:11 +0800

    added html, js and css files to weather app react project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import Weather from "./Weather";
+
+export default function App() {
+  return <h1> Weather App </h1>;
+  <Weather />;
+}
---
+++ src/Weather.js
@@ -0,0 +1,56 @@
+import react, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "b35c686ba9565ba0ab254c2230937552";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} placeholder="Enter a city.." />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
